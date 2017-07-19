ceb28d462467c8971cce30cb79c1afea65cc7a11
jczjdeveloper <jczjdeveloper@gmail.com>
2017-07-20 05:39:04 +0800

    changed backend port to 3001. Added proxy to frontend to connect to backend port. added in linkedin Auth passport strategy

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,24 @@ import passport from 'passport';
 import path from 'path';
 import lessMiddleware from 'less-middleware';
 import index from './routes/index';
-
+import auth from './routes/auth';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import session from 'express-session';
+
+const MongoStore = require('connect-mongo')(session);
+
+/**
+* Setup Mongoose connection to MongoDB
+*/
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/listtrdb', {
+                 useMongoClient: true,});
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
 
 const app = express();
 const debug = Debug('backend:app');
@@ -41,6 +57,38 @@ app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
+/**
+ * use EXPRESS-SESSION.
+ */
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'hello',
+  store: new MongoStore({
+    url: 'mongodb://localhost/listtrdb',
+    // url: process.env.MONGODB_URI || process.env.MONGODLAB_URI,
+    autoReconnect: true,
+    clear_interval: 3600
+  })
+  //cookie: {maxAge: 3600000}
+}));
+
+/**
+ * PASSPORT INITIALIZE AND SESSION.
+ */
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  console.log( "Method: " + req.method +" Path: " + req.url)
+  next();
+});
+
+/**
+ * Use AUTH routes.
+ */
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
