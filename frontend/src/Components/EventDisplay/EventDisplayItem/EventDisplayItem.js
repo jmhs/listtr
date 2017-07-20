ee9d987cd476625777dde96891cf00059634581c
dheamariesta <dhea23mar@gmail.com>
2017-07-20 11:21:21 +0800

    eventdisplayitem

---
+++ frontend/src/Components/EventDisplay/EventDisplayItem/EventDisplayItem.js
@@ -0,0 +1,19 @@
+import React, {PropTypes} from 'react';
+import './EventDisplayItem.css'
+import image from './event.jpg';
+class EventDisplayItem extends React.Component {
+
+  render() {
+    return (
+      <div className="event-item">
+        <div className="col-sm-12">
+          <div className="image">
+            <img src={image}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EventDisplayItem;
