51fbfe27b9fd2983aa54b29b1cba30664ef92925
dheamariesta <dhea23mar@gmail.com>
2017-07-20 13:16:50 +0800

    edit eventcontroller, can add event without pic

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -15,33 +15,24 @@ exports.getSpecificEvent = (req, res) => {
 }
 
 exports.postEvent = (req, res) => {
-  cloudinary.uploader.upload(req.file.path,(result) => {
-    const newEvent = new Event({
-      eventImage: result.secure_url || "",
-      eventName: req.body.name || "",
-      type: req.body.type || "" ,
-      description: req.body.description || "",
-      dressCode: req.body.dressCode || "",
-      location: req.body.location || "",
-      googleMapLink: req.body.googleMapLink || "",
-      startDate: req.body.startDate || "",
-      endDate: req.body.endDate || "",
-      timeStart: req.body.timeStart || "",
-      timeEnd: req.body.timeEnd || ""
-    });
+  const newEvent = new Event({
+    eventImage: result.secure_url || "",
+    eventName: req.body.name || "",
+    type: req.body.type || "" ,
+    description: req.body.description || "",
+    dressCode: req.body.dressCode || "",
+    location: req.body.location || "",
+    googleMapLink: req.body.googleMapLink || "",
+    startDate: req.body.startDate || "",
+    endDate: req.body.endDate || "",
+    timeStart: req.body.timeStart || "",
+    timeEnd: req.body.timeEnd || ""
+  });
 
-    newEvent.save((err) => {
-      if(err){console.log(err); return;}
-      res.json(newEvent);
-    });
-  })
-  .then(
-    fs.unlink(req.file.path, (err) => {
-      if (err) {
-            console.log("failed to delete local image:"+err);
-        } else {
-            console.log('successfully deleted local image');
-        }
-    })
-  );
+  newEvent.save((err) => {
+    if(err){console.log(err); return;}
+    res.json(newEvent);
+  });
+
+
 }
