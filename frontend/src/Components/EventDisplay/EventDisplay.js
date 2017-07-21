ac03485f7ee192d80f29eaf0b869a2bc5c7e319b
dheamariesta <dhea23mar@gmail.com>
2017-07-21 11:19:13 +0800

    setup redux for events. render eventdisplayitem in /eventdisplay

--- frontend/src/Components/EventDisplay/EventDisplay.js
+++ frontend/src/Components/EventDisplay/EventDisplay.js
@@ -1,24 +1,37 @@
 import React, {PropTypes} from 'react';
+
+import { connect } from 'react-redux'
 import EventDisplayItem from './EventDisplayItem/EventDisplayItem'
 class EventDisplay extends React.Component {
+  constructor(props) {
+    super(props);
 
+  }
+  renderEventDisplayItem = () => {
+    let events = this.props.events;
+    return events.map( (event) => {
+      return <EventDisplayItem eventName={event.eventName}/>
+    })
+  }
   render() {
+    const renderlist = this.renderEventDisplayItem();
     return (
       <div className="container">
         <div className="row">
-          <EventDisplayItem/>
-          <EventDisplayItem/>
-          <EventDisplayItem/>
-          <EventDisplayItem/>
-          <EventDisplayItem/>
-          <EventDisplayItem/>
-          <EventDisplayItem/>
-          <EventDisplayItem/>
-          <EventDisplayItem/>
+          {renderlist}
         </div>
       </div>
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    events: state.events
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
 
-export default EventDisplay;
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(EventDisplay);
--- frontend/src/Reducers/User.js
+++ frontend/src/Reducers/User.js
@@ -1,6 +1,6 @@
 const UserReducer = (state = {}, action) => {
 
-  console.log(action);
+  //console.log(action);
   switch (action.type) {
 
     case "USER_UPDATE":
@@ -8,7 +8,7 @@ const UserReducer = (state = {}, action) => {
 
     case "INIT_USER":
         return action.user;
-        
+
     default:
         return state;
   }
