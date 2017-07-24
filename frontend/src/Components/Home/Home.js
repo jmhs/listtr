df894dc8d836d10314b67ffcfc9a89f735bdf9f5
dheamariesta <dhea23mar@gmail.com>
2017-07-24 15:33:38 +0800

    homepage, account page

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
-
+import './Home.css'
+import {Link} from 'react-router-dom'
 class Home extends React.Component {
 
 
@@ -7,8 +8,26 @@ class Home extends React.Component {
 
 
     return (
-      <div className="container">
-        Home
+      <div>
+        <header>
+        <nav className="uk-navbar-container" uk-navbar>
+
+          <div className="uk-navbar-right">
+          <ul className="uk-navbar-nav">
+            <li><a href="#" id="navbar-title">Listtr</a></li>
+            <li><Link to="/login"><span  className="home-nav">Login</span></Link></li>
+            <li className="home-nav"><Link to="/signup"><span  className="home-nav">Sign Up</span></Link></li>
+          </ul>
+          </div>
+        </nav>
+
+
+        </header>
+        <section>
+          <div id="landing-page">
+            <h1 id="home-title">Listtr</h1>
+          </div>
+        </section>
       </div>
     );
   }
