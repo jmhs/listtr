afae2de570f919815f1875cb26badccb8d4ccb2f
Han <jmh.sng@gmail.com>
2017-07-26 16:16:15 +0800

    some basic card, commit first

--- frontend/src/Components/Preview/Preview.js
+++ frontend/src/Components/Preview/Preview.js
@@ -1,7 +1,8 @@
 import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom'
-import { activeEvent} from '../../Actions/Event'
+import { activeEvent} from '../../Actions/Event';
+import './Preview.css';
 class Preview extends React.Component {
   constructor(props) {
     super(props);
@@ -11,12 +12,38 @@ class Preview extends React.Component {
   // }
   render() {
     return (
-      <div>
-      <h2>{this.props.events}</h2>
-      <Link to="/guest"><button type="button"
+      <div className = "Firreee">
+      <div className ='card'>
+
+      <div className ='cardimage'>
+      <div className ="data-uk-parallax={bg: '-200'}">
+      <img src="https://res.cloudinary.com/listtr/image/upload/v1501038786/j6fbqeezvic6ej9isdj1.png" >
+      </img>
+      </div>
+      <div className="uk-height-large uk-background-cover uk-light uk-flex" uk-parallax="bgy: -200" style={{backgroundImage: 'url("images/dark.jpg")'}}>
+              <h1 className="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">Headline</h1>
+            </div>
+
+      </div>
+      <h2>HIHIHI</h2>
+      <Link to="/guest">
+      <button type="button"
               className="btn btn-default"
               onClick={this.onClick}>AddGuest</button></Link>
-      </div>
+
+
+        <h1 className="uk-article-title">
+        <a className="uk-link-reset" href> Headin </a> </h1>
+          <div>
+            <a className="uk-button uk-button-text" href="#">Read more</a>
+          </div>
+          <div>
+            <a className="uk-button uk-button-text" href="#">5 Comments</a>
+          </div>
+        </div>
+        </div>
+
+
   );
   }
 }
