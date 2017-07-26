f810e1fda472d47d0934c1b10b779e7578bf4efa
Han <jmh.sng@gmail.com>
2017-07-26 09:16:58 +0800

    nth much here, working on the click to show details/preview

--- frontend/src/Components/EventDisplay/EventDisplay.js
+++ frontend/src/Components/EventDisplay/EventDisplay.js
@@ -8,6 +8,11 @@ class EventDisplay extends React.Component {
     super(props);
 
   }
+
+  onClick = (id, active) => {
+  this.props.activeHome(active)
+  }
+  
   renderEventDisplayItem = () => {
     let events = this.props.events;
     console.log(events)
@@ -17,7 +22,8 @@ class EventDisplay extends React.Component {
       return <div><EventDisplayItem key={event._id}
                                id={event._id}
                                eventName={event.eventName}
-                               description={event.description}/></div>
+                               description={event.description}
+                               onClick={this.onClick}/></div>
     })
   }
   render() {
@@ -75,11 +81,12 @@ class EventDisplay extends React.Component {
 const mapStateToProps = (state) => {
   return {
     events: state.events
+    activeHome: state.activeHome
   }
 }
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    activeHome: (active) => {dispatch(activeHome(active));},
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(EventDisplay);
