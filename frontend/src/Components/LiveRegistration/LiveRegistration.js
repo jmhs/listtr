7b4b42d2a6a2e5fbaae31f57e6a943dd01531b10
Han <jmh.sng@gmail.com>
2017-07-28 16:21:03 +0800

    front end design for registrations

--- frontend/src/Components/LiveRegistration/LiveRegistration.js
+++ frontend/src/Components/LiveRegistration/LiveRegistration.js
@@ -7,19 +7,47 @@ export default class LiveRegistration extends React.Component {
   }
 
   render() {
-    const data = [
-  {d1: 'Data', d2: 'Data', d3: 'Data'},
-  {d1: 'Data', d2: 'Data', d3: 'Data'},
-  {d1: 'Data', d2: 'Data', d3: 'Data'}
-];
     return (
-      <div className ="row-col-6">
-      <Table
-        caption='Hoverable table'
-        hover head={['Heading', 'Heading', 'Heading']}
-        body={data}/>
-      </div>
+    <div className="uk-container uk-container-small uk-position-relative">
+    <div>
+    <h1>Registrations</h1>
+    </div>
+      <div className="uk-overflow-auto uk-position-relative uk-margin-medium" >
+              <table className="uk-table uk-table-hover uk-table-middle uk-table-divider">
+                <thead>
+                  <tr>
+                    <th className="uk-table-shrink">Attd</th>
+                    <th className="uk-table-shrink">Name</th>
+                    <th className="uk-table-expand">Email</th>
+                    <th className="uk-width-small">Contact No.</th>
+                    <th className="uk-table-shrink uk-text-nowrap">RSVP Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><input className="uk-checkbox" type="checkbox" /></td>
+                    <td><img className="uk-preserve-width uk-border-circle" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/17015b52218827.5909281cb99f2.jpg" width={40} alt /></td>
+                    <td className="uk-table-link">
+                      <a className="uk-link-reset" href>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                    </td>
+                    <td className="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                    <td className="uk-text-nowrap">Lorem ipsum dolor</td>
+                  </tr>
+                  <tr>
+                    <td><input className="uk-checkbox" type="checkbox" /></td>
+                    <td><img className="uk-preserve-width uk-border-circle" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/17015b52218827.5909281cb99f2.jpg" width={40} alt /></td>
+                    <td className="uk-table-link">
+                      <a className="uk-link-reset" href>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
+                    </td>
+                    <td className="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
+                    <td className="uk-text-nowrap">Lorem ipsum dolor</td>
+                  </tr>
 
+
+                </tbody>
+              </table>
+            </div>
+          </div>
     );
   }
 }
