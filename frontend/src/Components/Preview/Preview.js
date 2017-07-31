568c8cca1c8084b56bdf6bb553c22a29b12db53d
dheamariesta <dhea23mar@gmail.com>
2017-07-31 15:42:16 +0800

    halp

--- frontend/src/Components/Preview/Preview.js
+++ frontend/src/Components/Preview/Preview.js
@@ -30,40 +30,40 @@ class Preview extends React.Component {
   }
 
   componentDidMount(){
-
-    if(this.props.events.guests === []){
-      var bar = new ProgressBar.SemiCircle('#progress-bar', {
-        strokeWidth: 6,
-        color: '#FFEA82',
-        trailColor: '#eee',
-        trailWidth: 1,
-        easing: 'easeInOut',
-        duration: 6000,
-        svgStyle: null,
-        text: {
-          value: '',
-          alignToBottom: false
-        },
-        from: {color: '#FFEA82'},
-        to: {color: '#ED6A5A'},
-        // Set default step function for all animate calls
-        step: (state, bar) => {
-          bar.path.setAttribute('stroke', state.color);
-          var value = Math.round(bar.value() * 100);
-          if (value === 0) {
-            bar.setText('');
-          } else {
-            bar.setText(value);
-          }
-
-          bar.text.style.color = state.color;
-        }
-      });
-      bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-      bar.text.style.fontSize = '2rem';
-
-      bar.animate(1.0);
-    }
+    let events = this.props.events
+    // if(events.guests.length !== 0){
+    //   var bar = new ProgressBar.SemiCircle('#progress-bar', {
+    //     strokeWidth: 6,
+    //     color: '#FFEA82',
+    //     trailColor: '#eee',
+    //     trailWidth: 1,
+    //     easing: 'easeInOut',
+    //     duration: 6000,
+    //     svgStyle: null,
+    //     text: {
+    //       value: '',
+    //       alignToBottom: false
+    //     },
+    //     from: {color: '#FFEA82'},
+    //     to: {color: '#ED6A5A'},
+    //     // Set default step function for all animate calls
+    //     step: (state, bar) => {
+    //       bar.path.setAttribute('stroke', state.color);
+    //       var value = Math.round(bar.value() * 100);
+    //       if (value === 0) {
+    //         bar.setText('');
+    //       } else {
+    //         bar.setText(value);
+    //       }
+    //
+    //       bar.text.style.color = state.color;
+    //     }
+    //   });
+    //   bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    //   bar.text.style.fontSize = '2rem';
+    //
+    //   bar.animate(1.0);
+    // }
 
   }
 /*{this.props.response === "success" ? renderNotifyCreateSuccess : (<div>Not success</div>)}
