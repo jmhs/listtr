d5ef889636dd3e6d177a6ae52500209b7e9da554
Han <jmh.sng@gmail.com>
2017-07-31 09:37:01 +0800

    for merge

--- frontend/src/Components/LiveRegistration/LiveRegistration.js
+++ frontend/src/Components/LiveRegistration/LiveRegistration.js
@@ -7,19 +7,23 @@ import Table from 'react-uikit-table'
 
 
 
-export default class LiveRegistration extends React.Component {
+class LiveRegistration extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      
+    }
   }
 
   componentDidMount(){
+    this.props.active.
     const io = require('socket.io-client/dist/socket.io.js');
     const socket = io.connect('http://localhost:3001');
+    socket.emit('getAllGuests', "597dc2135f7bb128e8a42c79");
+
     socket.on('guestlist', function (data) {
     console.log(data);
-//    socket.emit('getAllGuests', "597dc2135f7bb128e8a42c79");
       });
-      socket.emit('getAllGuests', "597dc2135f7bb128e8a42c79");
   }
 
   render() {
@@ -49,18 +53,6 @@ export default class LiveRegistration extends React.Component {
                     <td className="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
                     <td className="uk-text-nowrap">Lorem ipsum dolor</td>
                   </tr>
-                  <tr>
-                    <td><input className="uk-checkbox" type="checkbox" /></td>
-                    <td><img className="uk-preserve-width uk-border-circle" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/17015b52218827.5909281cb99f2.jpg" width={40} alt /></td>
-                    <td className="uk-table-link">
-                      <a className="uk-link-reset" href>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</a>
-                    </td>
-                    <td className="uk-text-truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</td>
-                    <td className="uk-text-nowrap">Lorem ipsum dolor</td>
-                    <td><button class="uk-button uk-button-default" type="button">Button</button></td>
-                  </tr>
-
-
                 </tbody>
               </table>
             </div>
@@ -71,3 +63,16 @@ export default class LiveRegistration extends React.Component {
 
 LiveRegistration.propTypes = {
 };
+
+const mapStateToProps = (state) => {
+  return {
+    active: state.active,
+    user: state.user
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(LiveRegistration);
